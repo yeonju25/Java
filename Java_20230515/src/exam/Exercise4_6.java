823e55af41ebba6a204334c9d509bd9c934d6e6a
yeonju25 <lily_real@naver.com>
2023-05-16 18:00:45 +0900

    first commit

--- Java_20230515/src/exam/Exercise4_6.java
+++ Java_20230515/src/exam/Exercise4_6.java
@@ -4,9 +4,13 @@ public class Exercise4_6 {
 
 	public static void main(String[] args) {
 		// 두 개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의 수를 출력하는 프로그램을 작성하시오.
-		int[][] dice = new int[6][6];
-		for(int i = 0; i<dice.length; i++) {
-			
+		int[] dice1 = {1,2,3,4,5,6};
+		int[] dice2 = {1,2,3,4,5,6};
+		int j=0;
+		for(int i=0; i<dice1.length; i++) {
+			if(dice1[i]+dice2[j] == 6) {
+				System.out.println(dice1[i]+","+dice2[j]);
+			} 
 		}
 	}
 
